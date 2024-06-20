d20274bf7dfe8625d82c4eb944038e4d451be802
Darwin Mirko Tangalin <mirkochi98@gmail.com>
2024-06-20 19:02:21 +0800

    Final Final Commit

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Models\Product; // Replace with your actual Product model
+
+class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all(); // Fetch all products (adjust query as needed)
+
+        return view('products.index', ['products' => $products]);
+    }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,16 +1,13 @@
 <!-- resources/views/products/index.blade.php -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Products</title>
-</head>
-<body>
-    <h1>Products List</h1>
+
+@extends('layouts.app') <!-- Assuming you have a layout file -->
+
+@section('content')
+    <h1>Products</h1>
+
     <ul>
         @foreach($products as $product)
-            <li>{{ $product['name'] }} - ${{ $product['price'] }}</li>
+            <li>{{ $product->name }} - {{ $product->price }}</li>
         @endforeach
     </ul>
-</body>
-</html>
+@endsection
